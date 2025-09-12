fb799e35b7422bb17d0a890fc41bffc9365f0b18
devlaw-ware <devlaw.ware@gmail.com>
2025-09-11 21:01:44 -0300

    Adição mdc

---
+++ AED-II/mdc.c
@@ -0,0 +1,22 @@
+// Atividade 01 - send canvas 
+// Laura Beatriz Santos Viana 
+
+#include <stdio.h>
+
+int mdc(int a, int b) {
+    // Caso base: quando b é 0, retorna a mdc que foi encontrado 
+    if (b == 0) {
+        return a;
+    }
+    // aqui a gente faz a chamada da função recursiva 
+    return mdc(b, a % b);
+}
+
+int main() {
+    // aqui fazemos o testes
+    printf("MDC(50, 15) = %d\n", mdc(50, 15));  // Esperado: 5
+    printf("MDC(48, 18) = %d\n", mdc(48, 18));  // Esperado: 6
+    printf("MDC(101, 13) = %d\n", mdc(101, 13)); // Esperado: 1
+
+    return 0;
+}
