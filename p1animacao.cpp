b39902c1df71ac76781e6fdeb53ea11ed44b31cd
devlaw-ware <devlaw.ware@gmail.com>
2025-10-02 20:19:24 -0300

    adição quadrado freeglut

---
+++ p1animacao.cpp
@@ -0,0 +1,70 @@
+
+
+
+#include <GL/freeglut.h>
+#include <stdio.h>
+GLfloat x1 = 100.0f; //posicao inicial do quadrado
+GLfloat y1 = 150.0f;//posicao inicial do quadrado
+GLsizei rsize = 50; //distancia entre os verticies do quadrado
+// Vetor de translacao
+GLfloat xstep = 10.0f;  //transladar 3 unidades de medida para a direita 
+GLfloat ystep = 10.0f; //transladar 3 unidades de medida para cima 
+GLfloat windowWidth; // largura da janela
+GLfloat windowHeight;// altura da janela
+void Desenha(void)
+{
+glMatrixMode(GL_MODELVIEW);
+glLoadIdentity();
+glClear(GL_COLOR_BUFFER_BIT);
+glColor3f(1.0f, 0.0f, 0.0f);
+glBegin(GL_QUADS);
+glVertex2i(x1,y1+rsize);
+glVertex2i(x1,y1);
+glColor3f(0.0f, 0.0f, 1.0f);
+glVertex2i(x1+rsize,y1);
+glVertex2i(x1+rsize,y1+rsize);
+glEnd();
+glutSwapBuffers();
+}
+void Timer(int value)
+{
+// Muda a dire o quando chega na borda esquerda ou direita��
+if(x1 > windowWidth-rsize || x1 < 0)
+xstep = -xstep;
+// Muda a dire o quando chega na borda superior ou inferior��
+if(y1 > windowHeight-rsize || y1 < 0)
+ystep = -ystep;
+// Verifica o de bordas. Se a window for menor e o quadrado sair do volume de��
+
+if(x1 > windowWidth-rsize)
+x1 = windowWidth-rsize-1;
+if(y1 > windowHeight-rsize)
+y1 = windowHeight-rsize-1;
+x1 += xstep;
+y1 += ystep;
+glutPostRedisplay();
+glutTimerFunc(5,Timer, 0);
+}
+void AlteraTamanhoJanela(GLsizei w, GLsizei h)
+{
+printf("\n w: %d h: %d ", w, h);
+glViewport(0, 0, w, h);
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+windowWidth = w;
+windowHeight = h;
+gluOrtho2D(0.0f, windowWidth, 0.0f, windowHeight);
+}
+int main(int argc, char** argv)
+{
+glutInit(&argc,argv);
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+glutInitWindowSize(640,480);
+glutInitWindowPosition(10,10);
+glutCreateWindow("Animacao");
+glutDisplayFunc(Desenha);
+glutReshapeFunc(AlteraTamanhoJanela);// funcao callback para redimensionar a janela 
+glutTimerFunc(5, Timer, 0);
+glutMainLoop();
+}
